d31c620e92c0e3065c9c65213025361fab5b4323
hamed252252 <81410537+hamed252252@users.noreply.github.com>
2024-09-24 00:24:46 +0330

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
+import { Github, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    return (
+        <section className="container p-4 border border-black mx-auto mt-4">
+            <div className="w-3/4 h-0.5 mt-4 bg-black"></div>
+            <h1 className="font-bold text-3xl my-10">
+                Hamed Nazari
+            </h1>
+            <span className="text-primary font-semibold text-lg">
+                junior Frontend Developer
+            </span>
+            <div className="mt-6 ">
+                <p>Iran,Golestan Province</p>
+                <p>Aqqala</p>
+                <p>hmdnazari76@gmail.com</p>
+            </div>
+            <div className="flex flex-col gap-y-2 mt-6">
+                <p className="text-primary font-bold">
+                    Skills
+                </p>
+                <p className="min-w-96 w-full">
+                    HTML,CSS,JavaScript,Accesibilty,Figma to
+                    design,Responisve web Design,Tecnical
+                    Writing,Presentation
+                </p>
+            </div>
+            <div className="flex flex-col gap-y-2 mt-6">
+                <div className="text-primary font-bold">
+                    Education
+                </div>
+                <p className="min-w-96 w-full">
+                    <div className="font-semibold text-sky-600">
+                        Scool Name, Location -Degree
+                    </div>
+                    Month 20xx to Month 20xx List of Exiting
+                    Thing u did at University
+                </p>
+                <p className="text-primary font-bold">
+                    Education
+                </p>
+                <p className="min-w-96 w-full">
+                    <div className="font-semibold text-sky-600">
+                        Company Name,Location - Job Title
+                    </div>
+                    feb 2022 to fev 2024 List of Exiting
+                    <ul className="list-disc list-inside">
+                        <li>React</li>
+                        <li>Nextjs</li>
+                        <li>Figma</li>
+                    </ul>
+                    this is skills i gaind
+                </p>
+                <div className=" mt-6">
+                    <p className="text-primary font-semibold">
+                        Across the Internet
+                    </p>
+                    <p className="flex justify-start items-center gap-x-2">
+                        <Github />
+                        GitHub:{" "}
+                        <a
+                            href="https://github.com/hamed252252"
+                            className="text-blue-500"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            github.com/hamed252252
+                        </a>
+                    </p>
+                    <p className="flex justify-start items-center gap-x-2">
+                        <Linkedin />
+                        LinkedIn:{" "}
+                        <Link
+                            href="https://www.linkedin.com/in/hamed-nazari-106981216/"
+                            className="text-blue-500"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            linkedin.com/in/hamed-nazari-106981216
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
